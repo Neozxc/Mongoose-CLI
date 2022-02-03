1a5de5149dc8269be2204153b4c243ff08ae8934
Lapinas <mrlapinas99@gmail.com>
2022-02-03 15:04:23 +0000

    CRUD done with extra

--- src/app.js
+++ src/app.js
@@ -3,15 +3,19 @@ const mongoose = require("mongoose");
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
-const { addMovie, list } = require("./film/filmMethods");
+const { addMovie, list, update, deleteX, deleteY } = require("./film/filmMethods");
 
 const app = async () => {
     if (argv.add) {
         await addMovie({ name: argv.title, actor: argv.actor });
     } else if (argv.list) {
         list();
-    } else {
-        console.log("Wrong command");
+    } else if (argv.update) {
+        update();
+    } else if (argv.delete) {
+        deleteX();
+    } else if (argv.deleteY){
+        deleteY();
     }
 };
 
--- src/film/filmMethods.js
+++ src/film/filmMethods.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const FilmModel = require("./filmModel");
 
+// Create
 exports.addMovie = async (newFilm) => {
     try {
         let movie = new FilmModel(newFilm);
@@ -13,11 +14,49 @@ exports.addMovie = async (newFilm) => {
     mongoose.connection.close();
 };
 
+// Read
 exports.list = async () => {
     try {
         console.log(await FilmModel.find({}));
     } catch (error) {
         console.log(error);
+    }
+
+    mongoose.connection.close();
+};
+
+// Update
+exports.update = async () => {
+    try {
+        await FilmModel.updateOne({ name: "Clean", actor: "sdsd" });
+        console.log("Successfully updated!");
+    } catch (error) {
+        console.log(error);
+    }
+    
+    mongoose.connection.close();
+};
+
+// Delete 
+
+exports.deleteX = async () => {
+    try {
+        await FilmModel.deleteOne({ name: "Clean" });
+        console.log("Successfully deleted.");
+    } catch (error) {
+        console.log(error)
+    }
+
+    mongoose.connection.close();
+};
+
+// Delete related ones
+exports.deleteY = async () => {
+    try {
+        await FilmModel.deleteMany({ actor: "Actor not found." });
+        console.log("Successfully all related deleted.");
+    } catch (error) {
+        console.log(error)
     }
 
     mongoose.connection.close();
